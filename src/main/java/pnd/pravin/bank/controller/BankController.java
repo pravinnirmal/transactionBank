2a82f880bdcea55c45bf7f2e483de90c23cddc75
pravinnirmal <pravin.nirmal.doss@gmail.com>
2023-09-30 15:29:40 +0530

    saved user to db

--- src/main/java/pnd/pravin/bank/controller/BankController.java
+++ src/main/java/pnd/pravin/bank/controller/BankController.java
@@ -1,13 +1,19 @@
 package pnd.pravin.bank.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pnd.pravin.bank.BankEntitites.AddUserEntity;
+import pnd.pravin.bank.Bankservices.BankService;
 
 @Controller
 public class BankController {
+@Autowired
+    BankService bankService;
 
     @GetMapping("/")
     public String homePage(){
@@ -30,9 +36,11 @@ public class BankController {
         return "admin/adduser";
     }
 
-//    @PostMapping
-//    public String addUserToDb(AddUserEntity addUserEntity, Model model){
-//        return "user added";
-//    }
+    @PostMapping("admin/adduser")
+    public String addUserToDb(@ModelAttribute("AddUserEntity") AddUserEntity addUserEntity, Model model){
+        model.addAttribute("AddUserEntity", new AddUserEntity());
+        bankService.addUserToDatabase(addUserEntity);
+        return "/admin/registrationsuccess";
+    }
 
 }
