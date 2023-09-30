5cb85081d40f4302e9a7fd7295cdb30136d4e0d1
pravinnirmal <pravin.nirmal.doss@gmail.com>
2023-09-30 16:09:03 +0530

    added password encoder and update fkey in sql DB

--- src/main/java/pnd/pravin/bank/Bankservices/BankService.java
+++ src/main/java/pnd/pravin/bank/Bankservices/BankService.java
@@ -1,6 +1,7 @@
 package pnd.pravin.bank.Bankservices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pnd.pravin.bank.BankEntitites.AddUserEntity;
 import pnd.pravin.bank.BankRepositories.AddUserRepository;
@@ -10,8 +11,9 @@ public class BankService {
 @Autowired
     AddUserRepository addUserRepository;
     public void addUserToDatabase(AddUserEntity addUserEntity) {
-        String password = addUserEntity.getPassword();
-//        password
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(5);
+        addUserEntity.setPassword(encoder.encode(addUserEntity.getPassword()));
+        addUserEntity.setEnabled(1);
         addUserRepository.save(addUserEntity);
     }
 }
