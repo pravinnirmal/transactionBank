9195d640dac0ff7ba80fcca89fbbccd498e2aee5
pravinnirmal <pravin.nirmal.doss@gmail.com>
2023-10-02 11:45:25 +0530

    Working:User registration and login

---
+++ src/main/java/pnd/pravin/bank/BankRepositories/AddUserAuthorityRepository.java
@@ -0,0 +1,7 @@
+package pnd.pravin.bank.BankRepositories;
+
+import org.springframework.data.repository.CrudRepository;
+import pnd.pravin.bank.BankEntitites.AddUserAuthority;
+
+public interface AddUserAuthorityRepository extends CrudRepository <AddUserAuthority, String> {
+}
